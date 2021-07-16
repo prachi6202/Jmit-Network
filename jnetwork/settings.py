e9065c0f7d8623d3140979168f97836083d10520
prachi6202 <prachi6202@gmail.com>
2021-07-16 21:53:47 +0530

    edit

--- jnetwork/settings.py
+++ jnetwork/settings.py
@@ -152,9 +152,5 @@ LOGOUT_REDIRECT_URL = '/users/login/'
 
 #SMTP Configuration
 
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'jmitnetwork@gmail.com'
-EMAIL_HOST_PASSWORD = 'jmit@2021'
